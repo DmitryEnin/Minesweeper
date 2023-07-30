58ce338178e4df6ca09ba5d9663f194b78849269
Dmitry <enindmitry8@gmail.com>
2023-07-30 22:11:53 +1000

    Corrections in app.js line 35-45

--- app.js
+++ app.js
@@ -35,12 +35,12 @@ function createBoard() {
         if (squares[i].classList.contains('valid')) {
             if (i > 0 && !isLeftEdge && squares[i-1].classList.contains('bomb')) total ++
             if (i > 9 && !isRightEdge && squares[i + 1 - width].classList.contains('bomb')) total ++
-            if (i > 10 && squares[i - width].classList.contains('bomb')) total ++
-            if (i > 11 && !isLeftEdge && squares[i - 1 - width].classList.contains('bomb')) total ++
-            if (i < 98 && !isRightEdge && squares[i + 1].classList.contains('bomb')) total ++
-            if (i < 90 && !isLeftEdge && squares[i - 1 + width].classList.contains('bomb')) total ++
+            if (i > 9 && squares[i - width].classList.contains('bomb')) total ++
+            if (i > 10 && !isLeftEdge && squares[i - 1 - width].classList.contains('bomb')) total ++
+            if (i < 99 && !isRightEdge && squares[i + 1].classList.contains('bomb')) total ++
+            if (i < 91 && !isLeftEdge && squares[i - 1 + width].classList.contains('bomb')) total ++
             if (i < 88 && !isRightEdge && squares[i + 1 + width].classList.contains('bomb')) total ++
-            if (i < 89 && squares[i + width].classList.contains('bomb')) total ++
+            if (i < 90 && squares[i + width].classList.contains('bomb')) total ++
             squares[i].setAttribute('data', total)
         }
     }
@@ -60,7 +60,14 @@ createBoard()
 //click on squre action
 function click(square) {
     if (square.classList.contains('bomb')) {
-        alert ('Game over')
+        console.log('Game over')
+    } else {
+        let total = square.getAttribute('data')
+        if (total !=0) {
+            square.classList.add('checked')
+            square.innerHTML = total
+            return
+        }
     }
 }
 
